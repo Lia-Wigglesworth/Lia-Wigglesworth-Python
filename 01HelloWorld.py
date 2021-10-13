068a2c1c991c87955d96b082755cd779f0c8fc87
Lia-Wigglesworth <88805555+Lia-Wigglesworth@users.noreply.github.com>
2021-10-13 20:12:23 +1100

    Commit message

---
+++ 01HelloWorld.py
@@ -0,0 +1,8 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    01HelloWorld                                |")
+print("|    Name : Lia Wigglesworth                     |")
+print("|    Version : 01                                |")
+print("|                                                |") 
+print(" ------------------------------------------------")
+print("Hello World")
