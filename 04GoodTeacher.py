297267126b44d8371de2ce86d0443424ed9c80e4
Lia-Wigglesworth <88805555+Lia-Wigglesworth@users.noreply.github.com>
2021-10-21 09:04:55 +1100

    Add files via upload

---
+++ 04GoodTeacher.py
@@ -0,0 +1,12 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    04GoodTeacher                               |")
+print("|    Name : Lia Wigglesworth                     |")
+print("|    Version : 01                                |")
+print("|                                                |") 
+print(" ------------------------------------------------")
+teacherName = input("Teacher's name (try Mr Horan) ")
+if teacherName == "Mr Horan":
+    print("You are lucky, he is a great teacher.")
+else:
+    print(teacherName + " is an okay teacher")
