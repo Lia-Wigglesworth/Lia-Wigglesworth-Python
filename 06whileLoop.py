64e46932b3c42837f8e53b631efdd235a16b95e9
Lia-Wigglesworth <88805555+Lia-Wigglesworth@users.noreply.github.com>
2021-10-22 10:08:52 +1100

    Add files via upload

---
+++ 06whileLoop.py
@@ -0,0 +1,13 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    06whileLoop                                 |")
+print("|    Name : Lia Wigglesworth                     |")
+print("|    Version : 01                                |")
+print("|                                                |") 
+print(" ------------------------------------------------")
+subjectName = input("What is the name of this subject ")
+while subjectName != "IST":
+    input("Not Correct - try again")
+    break
+else:
+    print("Congratulations!!")
