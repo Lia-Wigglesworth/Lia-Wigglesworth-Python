96d3f803e480b8e06a7dfa1784315b28f710377b
Lia-Wigglesworth <88805555+Lia-Wigglesworth@users.noreply.github.com>
2021-10-22 09:13:44 +1100

    Add files via upload

--- 05forLoop.py
+++ 05forLoop.py
@@ -5,9 +5,7 @@ print("|    Name : Lia Wigglesworth                     |")
 print("|    Version : 01                                |")
 print("|                                                |") 
 print(" ------------------------------------------------")
-for x in range(501):
+for x in range(500):
   if x == 0:
-      continue
-  if x == 500:
       continue
   print(x)
